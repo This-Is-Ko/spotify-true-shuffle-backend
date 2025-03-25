15533a6beee8a5111dc0c51a43adf96101da6827
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2025-03-25 17:17:52 +1100

    Fix missing userid

--- app/tasks/task_state.py
+++ app/tasks/task_state.py
@@ -5,12 +5,15 @@ from classes.spotify_auth import SpotifyAuth
 
 
 def get_celery_task_state(spotify_auth: SpotifyAuth, id: str, type: str):
+    user_id = ""
     if not spotify_auth or not spotify_auth.user_id:
         current_app.logger.error("error=missing_user_id")
-        raise ValueError("Missing user_id in Spotify authentication")
+        # raise ValueError("Missing user_id in Spotify authentication")
+    else:
+        user_id = spotify_auth.user_id
 
     result = AsyncResult(id)
-    current_app.logger.info(f"type={type}, user_id={spotify_auth.user_id}, task_id={id}, state={result.state}")
+    current_app.logger.info(f"type={type}, user_id={user_id}, task_id={id}, state={result.state}")
 
     if result.state == "PROGRESS":
         return {
